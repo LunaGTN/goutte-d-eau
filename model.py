03147514a4ba36465a979242cfcf50da7f66e0ae
LunaGTN <gayton.marinho@gmail.com>
2026-03-20 09:56:26 +0100

    Normalisation des données numériques

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 #Entraîner un modèle de classification supervisé
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
 import sqlite3
 
 conn = sqlite3.connect("meteo.db")
@@ -11,8 +11,16 @@ df = pd.read_sql("SELECT * FROM meteo", conn)
 X = df.drop("pluie", axis = 1) #Les fetaures
 y = df["pluie"] # La cible
 
-#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-#model = LogisticRegression().fit(X_train, y_train)
+scaler = StandardScaler()
+scaler.fit(X_train)
+X_train_scaled = scaler.transform(X_train)
+X_test_scaled = scaler.transform(X_test)
 
-print(df.head(2))
+def fit_model(model):
+    model.fit(X_train_scaled, y_train)
+    y_pred = model.predict(X_test_scaled)
+    return y_test, y_pred
+
+
