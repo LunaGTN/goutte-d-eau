13fa7cccc1efeb333d0682a0b19fbc2213ff1a89
LunaGTN <gayton.marinho@gmail.com>
2026-03-20 17:10:33 +0100

    🐛 Import circulaire

--- call_api.py
+++ call_api.py
@@ -1,31 +1,7 @@
-from dotenv import load_dotenv
-import os
-import requests
+
 from fastapi import FastAPI
 from main import predict_rain_with_date
 
-#API_METEO
-BASE_URL = "https://www.infoclimat.fr/opendata/"
-
-load_dotenv()
-KEY = os.getenv("API_KEY")
-
-def get_data_from_api_now(date):
-    #date = str(datetime.now())[:10]
-    params = {
-    "version": 2,
-    "method": "get",
-    "format": "csv",
-    "stations[]": "07630",
-    "start": date,
-    "end": date,
-    "token": KEY
-}
-    response = requests.get(BASE_URL, params=params)
-    if response.status_code == 200:
-        return response.text
-    else:
-        return "Erreur :", response.status_code
 
 
 #MON_API
@@ -34,7 +10,7 @@ app = FastAPI()
 async def root():
     return {"message": "API qui renvoie une probabilité de pluie"}
 
-@app.post("/predict/")
+@app.get("/predict/")
 async def predict_rain(date):
     return {"rain_probability": predict_rain_with_date(date)}
 
--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 from clean_data import cleaning_csv
-from call_api import get_data_from_api_now
 from model import main_model
 from datetime import datetime
 from sklearn.ensemble import RandomForestClassifier
+from dotenv import load_dotenv
+import os
+import requests
+
+#API_METEO
+BASE_URL = "https://www.infoclimat.fr/opendata/"
+
+load_dotenv()
+KEY = os.getenv("API_KEY")
+
+def get_data_from_api_now(date):
+    #date = str(datetime.now())[:10]
+    params = {
+    "version": 2,
+    "method": "get",
+    "format": "csv",
+    "stations[]": "07630",
+    "start": date,
+    "end": date,
+    "token": KEY
+}
+    response = requests.get(BASE_URL, params=params)
+    if response.status_code == 200:
+        return response.text
+    else:
+        return "Erreur :", response.status_code
 
 
 model = RandomForestClassifier()
